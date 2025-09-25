00dc08fe0e68f02d69d55263cab3b31c161b215e
yeekong <yeekong.info@gmail.com>
2025-09-25 09:27:39 +0800

    (ADDED) Standalone library

--- STM32CubeIDE/Debug/Drivers/BSP/STM32U0xx_Nucleo/subdir.mk
+++ STM32CubeIDE/Debug/Drivers/BSP/STM32U0xx_Nucleo/subdir.mk
@@ -5,7 +5,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-C:/Users/yee-kong.chong/Documents/Development/STM32_ExampleCode/STM32Cube_FW_U0_V1.3.0/Drivers/BSP/STM32U0xx_Nucleo/stm32u0xx_nucleo.c 
+C:/Users/yee-kong.chong/STM32CubeIDE/workspace_U031/PWR_SHUTDOWN_Copy1/Drivers/BSP/STM32U0xx_Nucleo/stm32u0xx_nucleo.c 
 
 OBJS += \
 ./Drivers/BSP/STM32U0xx_Nucleo/stm32u0xx_nucleo.o 
@@ -15,8 +15,8 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Drivers/BSP/STM32U0xx_Nucleo/stm32u0xx_nucleo.o: C:/Users/yee-kong.chong/Documents/Development/STM32_ExampleCode/STM32Cube_FW_U0_V1.3.0/Drivers/BSP/STM32U0xx_Nucleo/stm32u0xx_nucleo.c Drivers/BSP/STM32U0xx_Nucleo/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32U031xx -c -I../../Inc -I../../../../../../../Drivers/BSP/STM32U0xx_Nucleo -IC:/Users/yee-kong.chong/STM32Cube/Repository/STM32Cube_FW_U0_V1.3.0/Drivers/STM32U0xx_HAL_Driver/Inc -IC:/Users/yee-kong.chong/STM32Cube/Repository/STM32Cube_FW_U0_V1.3.0/Drivers/STM32U0xx_HAL_Driver/Inc/Legacy -IC:/Users/yee-kong.chong/STM32Cube/Repository/STM32Cube_FW_U0_V1.3.0/Drivers/CMSIS/Device/ST/STM32U0xx/Include -IC:/Users/yee-kong.chong/STM32Cube/Repository/STM32Cube_FW_U0_V1.3.0/Drivers/CMSIS/Include -I"C:/Users/yee-kong.chong/Documents/Development/STM32_ExampleCode/STM32Cube_FW_U0_V1.3.0/Projects/NUCLEO-U031R8/Examples/PWR/PWR_SHUTDOWN_Copy/STM32CubeIDE/Application/User" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/BSP/STM32U0xx_Nucleo/stm32u0xx_nucleo.o: C:/Users/yee-kong.chong/STM32CubeIDE/workspace_U031/PWR_SHUTDOWN_Copy1/Drivers/BSP/STM32U0xx_Nucleo/stm32u0xx_nucleo.c Drivers/BSP/STM32U0xx_Nucleo/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32U031xx -c -I../../Inc -I"C:/Users/yee-kong.chong/STM32CubeIDE/workspace_U031/PWR_SHUTDOWN_Copy1/STM32CubeIDE/Application/User" -I../../Drivers/STM32U0xx_HAL_Driver/Inc -I../../Drivers/STM32U0xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32U0xx/Include -I../../Drivers/CMSIS/Include -I../../Drivers/BSP/STM32U0xx_Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Drivers-2f-BSP-2f-STM32U0xx_Nucleo
 
